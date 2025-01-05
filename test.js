3947b9d6cb135252eb23e83993745d9608e3714d
COOLSTORM <yoyo.sunshine@hotmail.com>
2025-01-05 22:31:47 +0000

    ok

---
+++ test.js
@@ -0,0 +1,47 @@
+const employees = [
+    {
+        id: 1,
+        name: 'Jim',
+    },
+    {
+        id: 2,
+        name: 'Dwight',
+    },
+    {
+        id: 3,
+        name: 'Pam',
+    },
+    {
+        id: 4,
+        name: 'Michael',
+    },
+    {
+        id: 5,
+        name: 'Ryan',
+    },
+    {
+        id: 6,
+        name: 'Andy',
+    },
+    {
+        id: 7,
+        name: 'Stanley',
+    },
+    {
+        id: 8,
+        name: 'Phyllis',
+    },
+    {
+        id: 9,
+        name: 'Meredith',
+    },
+    {
+        id: 10,
+        name: 'Creed',
+    }
+];
+
+// const employee = employees.find(function(e) {
+//     return e.name === 'Jim'
+// });
+// console.log(employee);
